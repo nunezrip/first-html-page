9b8f5ea60c33258375c30c2dba49b189874a4b5d
FVI Student <student@fvi.edu>
2017-08-30 21:43:15 -0400

    change from to use ajax

---
+++ app.js
@@ -0,0 +1,16 @@
+const contactForm = document.querySelector('#contact-form')
+
+contactForm.addEventListener('submit', function(e) {
+e.preventDefault()
+
+$.ajax({
+    url: contactForm.action,
+    method: contactForm.method,
+    data: $(contactForm).serialize()
+})
+
+}).done(function(response){
+    console.log(response)
+    
+})
+
